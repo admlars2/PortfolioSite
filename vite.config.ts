9da81e6ee6ae7e275c0c256ca52e6aa8c6aa75c7
Adam Larson <admlars2@gmail.com>
2026-03-20 15:52:50 -0700

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
